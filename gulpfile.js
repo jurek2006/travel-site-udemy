37c008ecc4bcf7ce7655dd3bb0098dbe74d491cc
jurek2006 <jurek2006@gmail.com>
2017-07-04 12:29:25 +0200

    Koniec lekcji 10. Gulp and PostCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-// gilpfile.js
+// gulpfile.js
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars');
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
@@ -15,7 +16,7 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssvars, autoprefixer]))
+		.pipe(postcss([cssvars, nested, autoprefixer]))
 		.pipe(gulp.dest('./app/temp/styles'));
 });
 
