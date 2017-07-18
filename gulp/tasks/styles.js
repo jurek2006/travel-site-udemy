89ad05f8220b4f0b2eda1a34203184596061e255
jurek2006 <jurek2006@gmail.com>
2017-07-18 11:43:22 +0200

    Czesc pierwsza - 35. Styling our header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,15 +1,16 @@
-// styles.js
+// styles.js w gulp/tasks
 var gulp = require('gulp'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+		.pipe(postcss([cssImport, cssvars, nested, hexrgba, autoprefixer]))
 		.on('error', function(errorInfo){
 			console.log(errorInfo.toString());
 			this.emit('end');
