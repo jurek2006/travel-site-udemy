4491b9327f7de5a140cf3b1f05ec83c299020cd2
jurek2006 <jurek2006@gmail.com>
2017-08-10 19:21:18 +0200

    Dodanie i konfiguracja Babel

--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -1,10 +1,14 @@
 // Person.js w app/assets/scripts/modules
-function Person(fullName, favColor) {
-	this.name = fullName;
-	this.favoriteColor = favColor;
-	this.greet = function(){
-		console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+class Person{
+	constructor(fullName, favColor){
+		this.name = fullName;
+		this.favoriteColor = favColor;
+	}
+
+	greet(){
+		console.log("Hi there, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
 	}
 }
 
-module.exports = Person;
+// module.exports = Person;
+export default Person;
