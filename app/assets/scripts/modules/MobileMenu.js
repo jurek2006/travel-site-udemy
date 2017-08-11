1bf0e28e31120d2953a5c461db35de656bf35002
jurek2006 <jurek2006@gmail.com>
2017-08-11 10:09:47 +0200

    Stylowanie mobilnego menu rozwijanego

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -5,6 +5,7 @@ import $ from 'jquery';
 
 class MobileMenu{
 	constructor(){
+		this.siteHeader = $(".site-header");
 		this.menuIcon = $(".site-header__menu-icon"); //wybranie ikony "hamburger menu"
 		this.menuContent = $(".site-header__menu-content"); //wybranie div menu (na mobile ukrytego)
 		this.events();
@@ -16,6 +17,7 @@ class MobileMenu{
 
 	toggleTheMenu(){
 		this.menuContent.toggleClass("site-header__menu-content--is-visible");
+		this.siteHeader.toggleClass("site-header--is-expanded");
 	}
 }
 
