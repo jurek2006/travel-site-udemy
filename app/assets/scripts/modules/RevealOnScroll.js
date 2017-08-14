9fad960b2df3e4f3d112ac7f37a7957b4490748c
jurek2006 <jurek2006@gmail.com>
2017-08-14 10:42:57 +0200

    Dodanie dzialajacego pokazywania elementow, gdy sie do nich doscrolluje

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,14 +1,30 @@
 // RevealOnScroll.js w /app/assets/scripts/modules
 import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll{
 	constructor(){
 		this.itemsToReveal = $(".feature-item");
 		this.hideInitially();
+		this.createWaypoints();
 	}
 
 	hideInitially(){
 		this.itemsToReveal.addClass("reveal-item");
+	}
+
+	createWaypoints(){
+		// this.itemsToReveal jest kolekcją czterech elementów i tworzymy waypoint dla kaźdego z tych elementów
+		this.itemsToReveal.each(function(){
+			const currentItem = this;
+			new Waypoint({
+				element: currentItem,
+				handler: function(){
+					$(currentItem).addClass("reveal-item--is-visible");
+				},
+				offset: "85%"
+			});
+		});
 	}
 }
 
